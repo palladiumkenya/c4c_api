0aaba37d48f22cf8792ce715b18df8ffa1823e47
Dennis Muoki <muoki@MHEALTHs-MacBook-Pro.local>
2020-03-13 09:24:28 +0300

    exposure with device

--- app/Exposure.php
+++ app/Exposure.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exposure extends Model
 {
-    //
+    public function device()
+    {
+        return $this->belongsTo('App\Device');
+    }
+
+    public function toArray() {
+        $data = parent::toArray();
+        $data['device'] = optional($this->device)->name;
+
+        return $data;
+    }
+
+
 }

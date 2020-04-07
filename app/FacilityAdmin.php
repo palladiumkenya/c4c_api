11cbe9c74f99c716025ce4579985b3fe4fbc135e
Dennis Muoki <muokid3@gmail.com>
2020-04-07 12:21:42 +0300

    facility admins

--- app/FacilityAdmin.php
+++ app/FacilityAdmin.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class FacilityAdmin extends Model
 {
-    //
+    public function facility()
+    {
+        return $this->belongsTo('App\Facility');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo('App\User','user_id');
+    }
+
+    public function toArray() {
+        $data = parent::toArray();
+        $data['facility_name'] = optional($this->facility)->name;
+        $data['mfl_code'] = optional($this->facility)->mfl_code;
+        $data['admin'] = $this->admin;
+
+        return $data;
+    }
+
 }
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Cadre;
 use App\CheckIn;
 use App\Disease;
+use App\Facility;
+use App\FacilityAdmin;
 use App\HealthCareWorker;
 use App\Http\Resources\GenericCollection;
 use App\Immunization;
@@ -41,5 +43,46 @@ class UserController extends Controller
     {
         return new GenericCollection(HealthCareWorker::where('facility_id',$id)->paginate(10));
     }
+
+    public function assign_facility_admin(Request $request)
+    {
+        $request->validate([
+            'facility_id' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        $facility = Facility::find($request->facility_id);
+        if (is_null($facility))
+            abort(404, "Facility does not exist");
+
+        $user = User::find($request->user_id);
+        if (is_null($user))
+            abort(404, "User does not exist");
+
+        $fAdmin = FacilityAdmin::where('facility_id',$request->facility_id)->where('user_id', $request->user_id)->first();
+        if (!is_null($fAdmin))
+            abort(403, "User is already assigned as a facility admin");
+
+        $facilityAdmin = new FacilityAdmin();
+        $facilityAdmin->facility_id = $request->facility_id;
+        $facilityAdmin->user_id = $request->user_id;
+        $facilityAdmin->saveOrFail();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User has been assigned as a facility admin successfully'
+        ], 201);
+    }
+
+    public function get_facility_admin($id)
+    {
+        $facility = Facility::find($id);
+        if (is_null($facility))
+            abort(404, "Facility does not exist");
+
+        return new GenericCollection(FacilityAdmin::where('facility_id', $id)->paginate(10));
+    }
+
+
 
 }

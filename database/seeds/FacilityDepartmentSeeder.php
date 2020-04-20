e7e6feb3a77e1a15999996ad6bb71d8f97dcd05c
Your Name <you@example.com>
2020-04-20 16:20:33 +0300

    departments

--- database/seeds/FacilityDepartmentSeeder.php
+++ database/seeds/FacilityDepartmentSeeder.php
@@ -14,6 +14,7 @@ class FacilityDepartmentSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('facility_departments')->truncate();
 
         $facilityDepartment = new \App\FacilityDepartment();
         $facilityDepartment->facility_id = 1;

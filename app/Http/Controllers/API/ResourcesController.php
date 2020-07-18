17d9597a232c2dbb89cecc84fdc76d004789be16
Dennis Muoki <muokid3@gmail.com>
2020-07-18 06:43:52 +0300

    make resources api public

--- app/Http/Controllers/API/ResourcesController.php
+++ app/Http/Controllers/API/ResourcesController.php
@@ -30,7 +30,7 @@ class ResourcesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+//        $this->middleware('auth:api');
     }
 
     public function diseases()
